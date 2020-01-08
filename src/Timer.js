d93109d7a0ff2552b470d21be2d227adc7c2e431
Amra Sezairi <amra.sezairi@hotmail.com>
2020-01-08 13:33:51 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -3,12 +3,20 @@ import React, { Component } from "react";
 class Timer extends Component {
   state = {
     time: 0,
-    color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+    color: "#" + Math.floor(Math.random() * 16777215).toString(16),
+    
   };
+  componentDidMount(){
+    this.interval = setInterval(this.clockTick,1000)
+  }
 
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
   // add your code here
 
   render() {
+    console.log(this.state.color)
     const { time, color } = this.state;
     return (
       <section className="Timer" style={{ background: color }}>
